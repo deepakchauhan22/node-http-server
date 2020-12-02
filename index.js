046d6d25bb42935f8ac33c51fdd48700ecd83707
deepak22 <deepakchauhan221997@gmail.com>
2020-12-03 01:30:09 +0530

    Node Http Server

--- index.js
+++ index.js
@@ -2,12 +2,47 @@ const http = require('http');
 
 const hostname = 'localhost';
 const port = 3000;
+const fs =require('fs');
+const path =require('path')
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello, World!</h1></body></html>');
+    //console.log(req.headers);
+    console.log('Request for ' + req.url + ' by method ' + req.method);
+    if(req.method == 'GET'){
+        var fileUrl;
+        if(req.url == '/')  fileUrl = '/index.html'
+        else fileUrl = req.url;
+        
+
+        var filePath= path.resolve('./public' + fileUrl);
+        const fileExt= path.extname(filePath);
+        if(fileExt == '.html'){
+            fs.exists(filePath, (exists)=>{
+                if(!exists){
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type','text/html')
+                    res.end('<html><body><h1> Error 404 '+fileUrl +'Not Found!</h1></body></html>')
+                    return;
+                }
+                res.statusCode =200;
+                res.setHeader('Content-Type','text/html');
+                fs.createReadStream(filePath).pipe(res);
+            })
+        }
+        else{
+            res.statusCode = 404;
+            res.setHeader('Content-Type','text/html')
+            res.end('<html><body><h1> Error 404 '+fileUrl +'Not an HTML Fle!</h1></body></html>')
+            return;
+
+        }
+    }
+    else{
+        res.statusCode = 404;
+        res.setHeader('Content-Type','text/html')
+        res.end('<html><body><h1> Error 404 '+ req.method + 'Not supported!</h1></body></html>')
+        return;
+    }
 })
 
 server.listen(port, hostname, () => {
@@ -20,3 +55,5 @@ server.listen(port, hostname, () => {
 //end endsthe response and sends response to client
 //server.listen will start the listening port
 //to use a variable in link use backquotes instead of normal ports ``
+//fs file system is a core node module allowsto read write file from stsem
+//path core module allows to specify local file path
